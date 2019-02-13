842e66c46feb378a0fbf37d3102cd0fc391ca2b8
Blaine <Ringospy@gmail.com>
2019-02-12 21:36:32 -0600

    TestingDemo: ASP.NET MVC 5

---
+++ TestingDemo/TestingDemo/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Blaine Smith
+ * Project walkthrough Due Tuesday, 02-12-2019
+ * NONE
+ * This program will test the methods created to see if they will provide the correct result.
+ */
+using System.Web.Mvc;
+
+namespace TestingDemo
+{
+    public class AdminController : Controller
+    {
+        private IUserRepository repository;
+
+        public AdminController(IUserRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult ChangeLoginName(string oldName, string newName)
+        {
+            User user = repository.FetchByLoginName(oldName);
+            user.LoginName = newName;
+            repository.SubmitChanges();
+            return View();
+        }
+    }
+}
---
+++ TestingDemo/TestingDemo/Models/Model.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Blaine Smith
+ * Project walkthrough Due Tuesday, 02-12-2019
+ * NONE
+ * This program will test the methods created to see if they will provide the correct result.
+ */
+namespace TestingDemo
+{
+    public class User
+    {
+        public string LoginName { get; set; }
+    }
+
+    public interface IUserRepository
+    {
+        void Add(User newUser);
+        User FetchByLoginName(string loginName);
+        void SubmitChanges();
+    }
+
+    public class DefaultUserRepository : IUserRepository
+    {
+
+        public void Add(User newUser)
+        {
+            
+        }
+
+        public User FetchByLoginName(string loginName)
+        {
+            return new User() { LoginName = loginName };
+        }
+
+        public void SubmitChanges()
+        {
+           
+        }
+    }
+}
